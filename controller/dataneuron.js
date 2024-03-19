d9d32af50cb42d477ce008d66b8a72d19121c756
abhishek mishra <abhms200@gmail.com>
2024-03-19 21:12:02 +0530

    add readme and add comment for better understanding

--- controller/dataneuron.js
+++ controller/dataneuron.js
@@ -1,10 +1,11 @@
 const componentmodal1 = require("../models/component1");
 const componentmodal2 = require("../models/component2");
 const componentmodal3 = require("../models/component3");
-const { performance } = require('perf_hooks');
+const { performance } = require("perf_hooks");
 const AllUser = {
   component1: async (req, res) => {
     try {
+      //here calulating execution time of API from line 9 to 12
       const startTime = performance.now();
       const endTime = performance.now();
       const executionTime = endTime - startTime;
@@ -14,19 +15,27 @@ const AllUser = {
       console.log(req.body, "pppppppp");
       // Find the current value of apiCall from the database
       let currentApiCall = 0; // Default value
-      const lastComponent = await componentmodal1.findOne().sort({ createdAt: -1 }).limit(1);
+      // i am getting value of api call of this table and then it will just increment to +1
+      const lastComponent = await componentmodal1
+        .findOne()
+        .sort({ createdAt: -1 })
+        .limit(1);
       if (lastComponent) {
         currentApiCall = lastComponent.apiCall;
       }
-      
+
       // Create a new document if not found, or update the existing one and increment apiCall by 1
-      const updatedData = await componentmodal1.findOneAndUpdate(
-        { firstname, lastname, email,apiCall: currentApiCall + 1 }, 
-     
-      );
-      res
-        .status(201)
-        .json({ message: "Data successfully pushed to the database" ,executionTime});
+      const updatedData = await componentmodal1.findOneAndUpdate({
+        firstname,
+        lastname,
+        email,
+        apiCall: currentApiCall + 1,
+      });
+      // sending response and execution time of API
+      res.status(201).json({
+        message: "Data successfully added in component 1",
+        executionTime,
+      });
     } catch (error) {
       console.error("Error updating course:", error);
       res.status(500).json({ error: "Internal server error" });
@@ -34,30 +43,38 @@ const AllUser = {
   },
   component2: async (req, res) => {
     try {
+      //here calulating execution time of API from line 48 to 50
       const startTime = performance.now();
       const endTime = performance.now();
       const executionTime = endTime - startTime;
       console.log(`API execution time: ${executionTime} milliseconds`);
       const { firstname, lastname, email } = req.body;
       console.log(req.body, "pppppppp");
-      
+
       // Find the current value of apiCall from the database
       let currentApiCall = 0; // Default value
-      const lastComponent = await componentmodal2.findOne().sort({ createdAt: -1 }).limit(1);
+      // i am getting value of api call of this table and then it will just increment to +1
+      const lastComponent = await componentmodal2
+        .findOne()
+        .sort({ createdAt: -1 })
+        .limit(1);
       if (lastComponent) {
         currentApiCall = lastComponent.apiCall;
       }
-      
-      // Create a new document if not found, or update the existing one and increment apiCall by 1
-      const updatedData = await componentmodal2.findOneAndUpdate(
-        { firstname, lastname, email,apiCall: currentApiCall + 1 }, 
-     
-      );
 
-      // Send a success response
-      res
-        .status(201)
-        .json({ message: "Data successfully pushed to the database",executionTime });
+      // Create a new document if not found, or update the existing one and increment apiCall by 1
+      const updatedData = await componentmodal2.findOneAndUpdate({
+        firstname,
+        lastname,
+        email,
+        apiCall: currentApiCall + 1,
+      });
+
+      // Send a success response and execution time of API
+      res.status(201).json({
+        message: "Data successfully added in component 2",
+        executionTime,
+      });
     } catch (error) {
       console.error("Error updating course:", error);
       res.status(500).json({ error: "Internal server error" });
@@ -65,30 +82,39 @@ const AllUser = {
   },
   component3: async (req, res) => {
     try {
+      //here calulating execution time of API from line 48 to 86
       const startTime = performance.now();
       const endTime = performance.now();
       const executionTime = endTime - startTime;
       console.log(`API execution time: ${executionTime} milliseconds`);
       const { firstname, lastname, email } = req.body;
       console.log(req.body, "pppppppp");
-      
+
       // Find the current value of apiCall from the database
       let currentApiCall = 0; // Default value
-      const lastComponent = await componentmodal3.findOne().sort({ createdAt: -1 }).limit(1);
+      // i am getting value of api call of this table and then it will just increment to +1
+
+      const lastComponent = await componentmodal3
+        .findOne()
+        .sort({ createdAt: -1 })
+        .limit(1);
       if (lastComponent) {
         currentApiCall = lastComponent.apiCall;
       }
-      
-      // Create a new document if not found, or update the existing one and increment apiCall by 1
-      const updatedData = await componentmodal3.findOneAndUpdate(
-        { firstname, lastname, email,apiCall: currentApiCall + 1 }, 
-     
-      );
 
-      // Send a success response
-      res
-        .status(201)
-        .json({ message: "Data successfully pushed to the database" ,executionTime});
+      // Create a new document if not found, or update the existing one and increment apiCall by 1
+      const updatedData = await componentmodal3.findOneAndUpdate({
+        firstname,
+        lastname,
+        email,
+        apiCall: currentApiCall + 1,
+      });
+
+      // Send a success response with execution time of API
+      res.status(201).json({
+        message: "Data successfully pushed to the database",
+        executionTime,
+      });
     } catch (error) {
       console.error("Error updating course:", error);
       res.status(500).json({ error: "Internal server error" });
@@ -96,15 +122,20 @@ const AllUser = {
   },
   allData: async (req, res) => {
     try {
+      //here calulating execution time of API from line 122 to 124
       const startTime = performance.now();
       const endTime = performance.now();
       const executionTime = endTime - startTime;
       console.log(`API execution time: ${executionTime} milliseconds`);
-      const comp1Data = await componentmodal1.find();
-      const comp2Data = await componentmodal2.find();
-      const comp3Data = await componentmodal3.find();
-      const allData = { comp1Data, comp2Data, comp3Data };
-      res.status(201).json({ message: "this is allData", allData ,executionTime});
+      const comp1Data = await componentmodal1.find(); // getting data of componentmodal1
+      const comp2Data = await componentmodal2.find(); // getting data of componentmodal2
+      const comp3Data = await componentmodal3.find(); // getting data of componentmodal3
+      const allData = { comp1Data, comp2Data, comp3Data }; //merging all data for final response
+
+      //sending all data of component 1 ,component 2 and component 3 with execution time of this API
+      res
+        .status(201)
+        .json({ message: "this is allData", allData, executionTime });
     } catch (error) {
       console.error("Error updating course:", error);
       res.status(500).json({ error: "Internal server error" });
@@ -113,12 +144,14 @@ const AllUser = {
 
   updateComp1: async (req, res) => {
     try {
+      //here calulating execution time of API from line 144 to 148
       const startTime = performance.now();
       const endTime = performance.now();
       const executionTime = endTime - startTime;
       console.log(`API execution time: ${executionTime} milliseconds`);
       const { firstname, lastname, email } = req.body;
       console.log(req.body);
+      // i am getting value of apiupdate call of this table and then it will just increment to +1
       const current = await componentmodal1.findOne();
       console.log(current, "ppppppppp");
       const updated = await componentmodal1.findOneAndUpdate({
@@ -127,7 +160,8 @@ const AllUser = {
         email,
         apiupdate: current.apiupdate + 1,
       });
-      res.status(201).json({ message: "updated",executionTime });
+      //sending success message with execution time of this API
+      res.status(201).json({ message: "Database updated of component 1", executionTime });
     } catch (error) {
       console.error("Error updating course:", error);
       res.status(500).json({ error: "Internal server error" });
@@ -141,6 +175,8 @@ const AllUser = {
       console.log(`API execution time: ${executionTime} milliseconds`);
       const { firstname, lastname, email } = req.body;
       console.log(req.body);
+      // i am getting value of apiupdate call of this table and then it will just increment to +1
+
       const current = await componentmodal2.findOne();
       console.log(current, "ppppppppp");
       const updated = await componentmodal2.findOneAndUpdate({
@@ -149,7 +185,8 @@ const AllUser = {
         email,
         apiupdate: current.apiupdate + 1,
       });
-      res.status(201).json({ message: "updated",executionTime });
+      //sending success message with execution time of this API
+      res.status(201).json({ message: "Database updated of component 2", executionTime });
     } catch (error) {
       console.error("Error updating course:", error);
       res.status(500).json({ error: "Internal server error" });
@@ -163,15 +200,17 @@ const AllUser = {
       console.log(`API execution time: ${executionTime} milliseconds`);
       const { firstname, lastname, email } = req.body;
       console.log(req.body);
+      // just trying to get the component3 api update value
       const current = await componentmodal3.findOne();
       console.log(current, "ppppppppp");
       const updated = await componentmodal3.findOneAndUpdate({
         firstname,
         lastname,
         email,
-        apiupdate: current.apiupdate + 1,
+        apiupdate: current.apiupdate + 1, //updating it to +1
       });
-      res.status(201).json({ message: "updated",executionTime });
+      //sending success message with execution time of this API
+      res.status(201).json({ message: "Database updated of component 3", executionTime });
     } catch (error) {
       console.error("Error updating course:", error);
       res.status(500).json({ error: "Internal server error" });
--- index.js
+++ index.js
@@ -17,13 +17,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Define routes
-app.post("/component1", AllUser.component1);
-app.post("/component2", AllUser.component2);
-app.post("/component3", AllUser.component3);
-app.get("/allData", AllUser.allData);
-app.post("/updateComp1", AllUser.updateComp1);
-app.post("/updateComp2", AllUser.updateComp2);
-app.post("/updateComp3", AllUser.updateComp3);
+app.post("/component1", AllUser.component1); //for adding data in component1
+app.post("/component2", AllUser.component2); //for adding data in component2
+app.post("/component3", AllUser.component3); //for adding data in component3
+app.get("/allData", AllUser.allData); //for getting all data from database
+app.post("/updateComp1", AllUser.updateComp1); // for updating data if component1
+app.post("/updateComp2", AllUser.updateComp2); // for updating data if component1
+app.post("/updateComp3", AllUser.updateComp3); // for updating data if component1
 
 // Start the server
 app.listen(port, () => {
